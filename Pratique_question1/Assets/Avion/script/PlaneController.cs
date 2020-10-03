882b5839e38b391234ae3f4f654f0a3a5f99d219
ouzinem@hotmail.com <ouzinem@hotmail.com>
2020-10-03 17:57:57 -0400

    helice et vitesse

---
+++ Pratique_question1/Assets/Avion/script/PlaneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    public float speed = 700.0f;
+    private Rigidbody my_rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        my_rigidbody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vitesse = Input.GetAxis("Jump");
+        if (vitesse!=0)
+        {
+
+            Helice(vitesse);
+        }
+    }
+    private void Helice(float acceleration)
+    {
+        my_rigidbody.AddForce(Vector3.forward * speed * Time.deltaTime);
+    }
+}
