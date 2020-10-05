60523a7b488a965ac038614c4efe6073ac407ca9
OusseynouMbaye <60521947+OusseynouMbaye@users.noreply.github.com>
2020-10-05 11:34:59 -0400

    presque

---
+++ Pratique_question1/Assets/Avion/script/Helice.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 700.0f;
+    public GameObject helice;
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vitesse = Input.GetAxis("Jump");
+
+        helice.transform.Rotate( new Vector3 ( 0, 0, vitesse * speed * Time.deltaTime) );
+    }
+}
--- Pratique_question1/Assets/Avion/script/PlaneController.cs
+++ Pratique_question1/Assets/Avion/script/PlaneController.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlaneController : MonoBehaviour
 {
     public float speed = 700.0f;
+    public float decolle = 200.0f;
     private Rigidbody my_rigidbody;
-
+    public ParticleSystem m_fumee;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +20,38 @@ public class PlaneController : MonoBehaviour
     void Update()
     {
         float vitesse = Input.GetAxis("Jump");
+        float boost = Input.GetAxis("Vertical");
         if (vitesse!=0)
         {
 
             Helice(vitesse);
         }
+
+        if (boost != 0) {
+            my_rigidbody.AddForce(Vector3.up * decolle * Time.deltaTime);
+
+            if (m_fumee.isStopped && !m_fumee.isPlaying)
+            {
+                m_fumee.Play();
+            }
+
+        }else
+        {
+            if ( m_fumee.isPlaying)
+            {
+                m_fumee.Stop();
+            }
+        }
+
     }
     private void Helice(float acceleration)
     {
         my_rigidbody.AddForce(Vector3.forward * speed * Time.deltaTime);
     }
 }
+/*
+   if (m_Smoke.isStopped && !m_Smoke.isPlaying)
+        {
+            m_Smoke.Play();
+        }
+        */
